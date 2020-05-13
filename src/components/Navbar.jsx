d0d4550a020cccfb996bd5e73bed436852a58863
Yousri <laifayousri@gmail.com>
2020-05-14 00:09:28 +0200

    Navbar component created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/works">Works</Link>
+      </nav>
+    </>
+  );
+};
+export default Navbar;
