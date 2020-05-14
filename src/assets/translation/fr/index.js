c03082161a3e7df5476c18f296e62967e4b60068
Yousri <laifayousri@gmail.com>
2020-05-14 19:37:10 +0200

    add translation data

---
+++ src/assets/translation/fr/index.js
@@ -0,0 +1,11 @@
+import works from "./works.json";
+import about from "./about.json";
+import home from "./home.json";
+
+const en = {
+    ...works,
+    ...about,
+    ...home,
+};
+
+export default en;
